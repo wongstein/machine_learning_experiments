ab0e6079e3c072b96b0dd49c442574f61e4a5ffe
Amee Wongstein <awang1596@gmailcom>
2016-04-24 19:07:00 +0800

    add threading for data setup

--- point_of_view_machine_learning.py
+++ point_of_view_machine_learning.py
@@ -5,6 +5,8 @@ import datetime
 import sys
 #import PCA_features
 import time
+import pandas as pd
+from multiprocessing import Process, Queue
 
 '''
 purpose: test to see if random forest can make predictions for
@@ -35,33 +37,83 @@ def transform_data(data_list, transformation_model):
 
     return transformed_data
 
-def fill_training_and_testing_data(features_to_use, testing_dates, training_dates = None):
-    global all_data
+def shape_listing_data(listing_id, listing_all_data, testing_dates, training_dates, q):
 
+    all_days = listing_all_data['day'].values.tolist()
+
+    testing_data = {listing_id: {"features": [], "classification" : []}}
+    training_data = {"features": [], "classification" : []}
+
+    for string_day in all_days:
+        day = datetime.datetime.strptime(string_day, "%Y-%m-%d").date()
+        day_features = all_data.loc[(all_data['listing_id']==listing_id) & (all_data['day'] == string_day)][features_to_use].values.tolist()[0]
+        classification = all_data.loc[(all_data['listing_id']==listing_id) & (all_data['day'] == string_day)]["classification"].values.tolist()[0]
+
+        if day < testing_dates['start_date'] and day >= training_dates['start_date']:
+            training_data['features'].append(day_features)
+            training_data['classification'].append(classification)
+        elif day <= testing_dates['end_date']:
+            testing_data[listing_id]['features'].append(day_features)
+            testing_data[listing_id]['classification'].append(classification)
+
+    q.put((training_data, testing_data))
+
+
+def fill_training_and_testing_data(features_to_use, testing_dates, training_dates):
+    global all_data
+    testing_listings = list(set(all_data['listing_id'].tolist()))
 
     training_data = {"features": [], "classification" : []}
     testing_data = {}
-    {"features": [], "classification" : []}
+
+    thread_number = 5
+    listing_chunks = []
 
     #listing_id will be a string
-    for listing_id in set(all_data['listing_id'].tolist()):
-        all_days = all_data.loc[all_data['listing_id'] == listing_id]['day'].values.tolist()
+    import time
 
-        testing_data[listing_id] = {"features": [], "classification" : []}
+    start_time = time.time()
 
-        for string_day in all_days:
-            day = datetime.datetime.strptime(string_day, "%Y-%m-%d").date()
-            day_features = all_data.loc[(all_data['listing_id']==listing_id) & (all_data['day'] == string_day)][features_to_use].values.tolist()
-            classification = all_data.loc[(all_data['listing_id']==listing_id) & (all_data['day'] == string_day)]["classification"].values.tolist()[0]
+    for x in xrange(0, len(testing_listings), thread_number):
+        small_chunk = []
+        for y in xrange(0, thread_number, 1):
+            try:
+                small_chunk.append(testing_listings[x + y])
+            except IndexError: #then it's finished
+                listing_chunks.append(small_chunk)
+                continue
+        listing_chunks.append(small_chunk)
 
-            if day < testing_dates['start_date'] and day >= training_dates['start_date']:
-                training_data['features'].append(day_features)
-                training_data['classification'].append(classification)
-            elif day <= testing_dates['end_date']:
-                testing_data[listing_id]['features'].append(day_features)
-                testing_data[listing_id]['classification'].append(classification)
+    for id_chunk in listing_chunks:
+        q = Queue()
+        process_list = []
+
+        for listing_id in id_chunk:
+            p = Process(target = shape_listing_data, args = (listing_id, all_data.loc[(all_data['listing_id']==listing_id)], testing_dates, training_dates, q))
+            process_list.append(p)
+            p.start()
+
+        process_list[0].join #ensure we wait at least for the first thread to finish before getting results
+
+        for process_tuple in process_list:
+            #training dict then testing_dict
+            return_tuple = q.get()
+            training_data['features'] += return_tuple[0]['features']
+            training_data['classification'] += return_tuple[0]['classification']
+
+            testing_data = _merge_dicts(testing_data, return_tuple[1])
+
+            process_tuple.join
+
+
+    print "time took: ", (time.time() - start_time)
 
     return (training_data, testing_data)
+
+def _merge_dicts(dict_1, dict_2):
+    z = dict_1.copy()
+    z.update(dict_2)
+    return z
 
 '''
 Classification part of code
@@ -167,7 +219,6 @@ def fullLocation(experiment_name, features_to_use = None, normalisation_type = N
         training_data = classification_data[0]
         testing_data = classification_data[1]
 
-
         #transform data
         #
         #try making normalisation just on the training data (most realistic)
@@ -221,7 +272,8 @@ def fullLocation(experiment_name, features_to_use = None, normalisation_type = N
         print "saved average results"
 
         print analysis
-        print "analyzed ", len(all_results), " records"
+        print "\nanalyzed ", len(all_results), " records"
+        print "time taken: ", (time.time() - start_time)
     print "finished!"
 
 def point_of_view_experiments(experiment, features_to_use):
@@ -235,8 +287,9 @@ def point_of_view_experiments(experiment, features_to_use):
 
 if __name__ == '__main__':
 
-    experiment = "full_location_point_of_view_best_match_average_"
-    point_of_view_experiments(experiment)
+    experiment = "full_location_point_of_view_optimised_"
+    features_to_use = ["weekday_number","week_number","quarter_in_year","day_number","month_number","listing_cluster","days_active","price_dict","cancellation count","enquiry count","k_means_season_day","season_-1","season_-2","season_-3","season_-4","season_-5","season_-6","season_-7","listing_cluster_avg_occupancy_best_match_day_-1","listing_cluster_avg_occupancy_best_match_day_-2","listing_cluster_avg_occupancy_best_match_day_-3","listing_cluster_avg_occupancy_best_match_day_-4","listing_cluster_avg_occupancy_best_match_day_-5","listing_cluster_avg_occupancy_best_match_day_-6","listing_cluster_avg_occupancy_best_match_day_-7","occupancy_match_average","enquiry_match_average","cancelled_match_average","occupancy_match_avg_-1","occupancy_match_avg_-2","occupancy_match_avg_-3","occupancy_match_avg_-4","occupancy_match_avg_-5","enquiry_match_avg_-1","enquiry_match_avg_-2","enquiry_match_avg_-3","enquiry_match_avg_-4","enquiry_match_avg_-5","cancelled_match_avg_-1","cancelled_match_avg_-2","cancelled_match_avg_-3","cancelled_match_avg_-4","cancelled_match_avg_-5","occupancy_match_avg_+1","occupancy_match_avg_+2","occupancy_match_avg_+3","occupancy_match_avg_+4","occupancy_match_avg_+5","enquiry_match_avg_+1","enquiry_match_avg_+2","enquiry_match_avg_+3","enquiry_match_avg_+4","enquiry_match_avg_+5","cancelled_match_avg_+1","cancelled_match_avg_+2","cancelled_match_avg_+3","cancelled_match_avg_+4","cancelled_match_avg_+5"]
+    point_of_view_experiments(experiment, features_to_use)
 
 
 
