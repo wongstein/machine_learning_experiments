d63846a0354a52f3466b5f2b743b88c5b7b43e8f
Amee Wongstein <awang1596@gmailcom>
2016-05-04 12:40:18 +0800

    Only neural network experiments

--- main.py
+++ main.py
@@ -29,24 +29,23 @@ import point_of_view_machine_learning
 import point_of_view_neural
 
 #date base
-'''
+
 experiment = "date_base_"
 features_to_use = ["weekday_number","week_number","quarter_in_year","day_number","month_number","listing_cluster","days_active","price_dict","cancellation count","enquiry count","k_means_season_day","season_-1","season_-2","season_-3","season_-4","season_-5","season_-6","season_-7"]
-point_of_view_machine_learning.point_of_view_experiments(experiment, features_to_use)
+#point_of_view_machine_learning.point_of_view_experiments(experiment, features_to_use)
 point_of_view_neural.point_of_view_experiments(experiment, features_to_use, "simple_network")
 
 #Increment
 experiment = "date_base_best_match_average"
 features_to_use = ["weekday_number","week_number","quarter_in_year","day_number","month_number","listing_cluster","days_active","price_dict","cancellation count","enquiry count","k_means_season_day","season_-1","season_-2","season_-3","season_-4","season_-5","season_-6","season_-7", "occupancy_match_average","enquiry_match_average","cancelled_match_average"]
-point_of_view_machine_learning.point_of_view_experiments(experiment, features_to_use)
+#point_of_view_machine_learning.point_of_view_experiments(experiment, features_to_use)
 point_of_view_neural.point_of_view_experiments(experiment, features_to_use, "simple_network")
-'''
 
 #what I think will do best right now
 experiment = "full_location_point_of_view_optimised_"
 features_to_use = ["weekday_number","week_number","quarter_in_year","day_number","month_number","listing_cluster","days_active","price_dict","cancellation count","enquiry count","k_means_season_day","season_-1","season_-2","season_-3","season_-4","season_-5","season_-6","season_-7","occupancy_match_average","enquiry_match_average","cancelled_match_average","occupancy_match_avg_-1","occupancy_match_avg_-2","occupancy_match_avg_-3","occupancy_match_avg_-4","occupancy_match_avg_-5","enquiry_match_avg_-1","enquiry_match_avg_-2","enquiry_match_avg_-3","enquiry_match_avg_-4","enquiry_match_avg_-5","cancelled_match_avg_-1","cancelled_match_avg_-2","cancelled_match_avg_-3","cancelled_match_avg_-4","cancelled_match_avg_-5","occupancy_match_avg_+1","occupancy_match_avg_+2","occupancy_match_avg_+3","occupancy_match_avg_+4","occupancy_match_avg_+5","enquiry_match_avg_+1","enquiry_match_avg_+2","enquiry_match_avg_+3","enquiry_match_avg_+4","enquiry_match_avg_+5","cancelled_match_avg_+1","cancelled_match_avg_+2","cancelled_match_avg_+3","cancelled_match_avg_+4","cancelled_match_avg_+5"]
-point_of_view_machine_learning.point_of_view_experiments(experiment, features_to_use)
-#point_of_view_neural.point_of_view_experiments(experiment, features_to_use, "simple_network")
+#point_of_view_machine_learning.point_of_view_experiments(experiment, features_to_use)
+point_of_view_neural.point_of_view_experiments(experiment, features_to_use, "simple_network")
 
 
 
